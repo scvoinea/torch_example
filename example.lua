25f9c91f56d02f07321af7d5feac08b8d2ed7689
scvoinea <scvoinea@gmail.com>
2015-12-14 19:19:09 -0500

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,13 +1,17 @@
 s = torch.LongTensor{5,6}
+
 m = nn.Sequential()
 p = nn.Parallel(2,2)
+
 for i = 1,2 do
     p:add(nn.LookupTable(s[i], 3))
 end
+
 m:add(p)
 m:add(nn.Linear(6,3))
 m:add(nn.LogSoftMax())
-for i = 1,1000 do
+    
+for i = 1,10 do
     x = torch.Tensor(3,1)
     x:random(5)
     t = torch.Tensor(3,1)
